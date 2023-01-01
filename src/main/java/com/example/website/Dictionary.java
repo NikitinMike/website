7e96db011ed0cad20d1d16e2e71cbea3ccf3bd21
mike <jobles11@gmail.com>
2023-01-01 21:47:49 +0300

    read lopatin wordbook dictionary

--- src/main/java/com/example/website/Dictionary.java
+++ src/main/java/com/example/website/Dictionary.java
@@ -3,6 +3,7 @@ package com.example.website;
 import java.io.File;
 import java.util.Hashtable;
 
+import static com.example.website.DataStreams.readLopatinFile;
 import static com.example.website.DataStreams.readWordBookFile;
 
 //@Repository
@@ -10,9 +11,13 @@ public class Dictionary {
     static Hashtable<String, String> wordTable = new Hashtable<>();
 
     public Dictionary() {
-        if (wordTable.isEmpty())
-            for (String word : readWordBookFile(new File("D:\\DBWords\\wordbook.txt")))
+        if (wordTable.isEmpty()){
+            for (String word : readWordBookFile(new File("\\DBWords\\wordbook.txt")))
                 wordTable.put(word.trim().replaceAll("'", ""), word);
+            for(String word:readLopatinFile(new File("\\DBWords\\lop1v2.utf8.txt")))
+                wordTable.put(word.trim().replaceAll("`", ""), word);
+        }
+//        System.out.println("Dictionary:" + wordTable.get("только"));
         System.out.println("Dictionary words:" + wordTable.size());
 //        wordTable.forEach((k,v) -> System.out.print(k + ":"+v+","));
     }
@@ -21,7 +26,7 @@ public class Dictionary {
 //        System.out.println(word);
         if (wordTable.containsKey(word)) word = wordTable.get(word);
         //            if (word.matches(".*([её]).*"))
-        if (!word.contains("'"))
+        if (!word.contains("`")&&!word.contains("'"))
             if (word.replaceAll("[^ёуеыаоэяию]+", "").length() == 1)
                 word = word.replaceFirst("([ёуеыаоэяию])", "$1'");
             else word = word.replaceFirst("([ёе])", "$1'");
