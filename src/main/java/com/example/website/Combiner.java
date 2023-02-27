4130c24e0fc5e978ab0bab92d46b55752a647d83
mike <jobles11@gmail.com>
2023-02-27 16:14:37 +0300

    pull out all combinations

--- src/main/java/com/example/website/Combiner.java
+++ src/main/java/com/example/website/Combiner.java
@@ -7,17 +7,19 @@ import static java.lang.Math.random;
 public class Combiner {
 
     int amount;
-    int[][] sentence;
+    int[][] combines;
+
+    public int[][] getCombines() {return combines;}
 
     public Combiner(int length) {
-        sentence = new int[factorial(length)][length];
-        for (int i = 0; i < length; i++) sentence[0][i] = i;
+        combines = new int[factorial(length)][length];
+        for (int i = 0; i < length; i++) combines[0][i] = i;
         amount = length > 1 ? combInit(length) : 1;
     }
 
-    public int[] randomSentence(int v) {
-        if (v > 0 && amount > 1) return sentence[(int) (random() * amount)];
-        return sentence[0];
+    public int[] getRandom(int v) {
+        if (v > 0 && amount > 1) return combines[(int) (random() * amount)];
+        return combines[0];
     }
 
     int combInit(int n) {
@@ -25,11 +27,11 @@ public class Combiner {
             int nf = combInit(n - 1);
             for (int i = 0; i < nf; i++)
                 for (int j = 1; j < n; j++)
-                    sentence[nf * j + i] = swapClone(sentence[nf * (j - 1) + i], n - j);
+                    combines[nf * j + i] = swapClone(combines[nf * (j - 1) + i], n - j);
             return nf * n;
         }
         // N=2
-        sentence[1] = swapClone(sentence[0], 1);
+        combines[1] = swapClone(combines[0], 1);
         return 2;
     }
 }
